ad3a22ca126931a013bebd9a51425398e0f76f49
Philip Brown <phil@ipbrown.com>
2013-06-23 14:35:29 +0100

    Initial commit

---
+++ src/Magniloquent/Magniloquent/MagniloquentServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Magniloquent\Magniloquent;
+
+use Illuminate\Support\ServiceProvider;
+
+class MagniloquentServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+/**
+   * Bootstrap the application events.
+   *
+   * @return void
+   */
+  public function boot()
+  {
+    $this->package('magniloquent/magniloquent');
+  }
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array('magniloquent');
+	}
+
+}
