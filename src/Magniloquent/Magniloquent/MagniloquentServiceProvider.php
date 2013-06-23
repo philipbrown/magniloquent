e051145b819d8b0deb719d0d1195a052d795d1ff
Philip Brown <phil@ipbrown.com>
2013-06-23 15:27:33 +0100

    Clean up Service Provider

--- src/Magniloquent/Magniloquent/MagniloquentServiceProvider.php
+++ src/Magniloquent/Magniloquent/MagniloquentServiceProvider.php
@@ -28,15 +28,7 @@ class MagniloquentServiceProvider extends ServiceProvider {
 	 */
   public function register()
   {
-    $this->app['magniloquent'] = $this->app->share(function($app)
-    {
-      return new Magniloquent;
-    });
-    $this->app->booting(function()
-    {
-      $loader = \Illuminate\Foundation\AliasLoader::getInstance();
-      $loader->alias('Magniloquent', 'Magniloquent\Magniloquent\Facades\Magniloquent');
-    });
+
   }
 
 	/**
